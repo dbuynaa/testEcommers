88a65b99af87743d34114b6d23b2cbba420b62a4
Baterdene <hashbaterdene@gmail.com>
2023-01-03 18:05:55 +0800

    try cash amount

--- src/modules/checkout/PaymentContainer.tsx
+++ src/modules/checkout/PaymentContainer.tsx
@@ -65,7 +65,7 @@ const PaymentContainer = ({
             variables: {
               id: orderId,
               doc: {
-                mobileAmount: parseFloat(totalAmount),
+                cashAmount: parseFloat(totalAmount),
               },
             },
           });
