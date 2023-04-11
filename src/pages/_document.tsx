03ade31d783fed0e104e01a6e4f418adaa308636
KhBaterdene <baterdne.k@gmail.com>
2023-04-11 18:39:32 +0800

    tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,7 +26,7 @@ export default class MyDocument extends Document {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KWD7T33')`,
+            })(window,document,'script','dataLayer','GTM-PKNHQBP')`,
             }}
           />
         </Head>
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
           <NextScript />
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWD7T33"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PKNHQBP"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
