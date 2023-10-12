92acacdc813ded63161b512df846c1279649c21d
uuganbayar.b <uuganbayar.b@nmma.co>
2023-10-12 13:43:54 +0800

    update SEO

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,10 +1,8 @@
-import { content } from "lib/Settings";
-import Image from "next/image";
+import { content } from 'lib/Settings';
+// import Image from "next/image";
 
 const Logo = ({ ...props }) => {
-  return (
-    <img src={content.logo} {...props} alt="logo" width={400} height={400} />
-  );
+  return <img src={content.logo} {...props} alt="logo" width={400} height={400} />;
 };
 
 export default Logo;
